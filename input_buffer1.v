0aadda0d234a51f8807caf2aed745845c386413f
SenjuBharani <137296303+BharaneswarT@users.noreply.github.com>
2025-09-16 14:43:03 +0530

    Update input_buffer successfully

--- input_buffer1.v
+++ input_buffer1.v
@@ -1,16 +1,17 @@
 module input_buffer1 #(
-    parameter DATA_WIDTH = 12,
+    parameter DATA_WIDTH = 16,
     parameter BUFFER_SIZE = 256
 )(
     input clk,
     input reset,
     input valid_in,
     input [DATA_WIDTH-1:0] sample_in,
-    output reg ready_for_processing
+    output reg ready_for_processing,
+    output logic [DATA_WIDTH*BUFFER_SIZE-1:0] buffer_flat  //  Packed output
 );
 
-    reg [DATA_WIDTH-1:0] buffer [0:BUFFER_SIZE-1];   //storage, which has the samples init
-    reg [$clog2(BUFFER_SIZE):0] write_ptr;       // where to store the next samples
+    reg [DATA_WIDTH-1:0] buffer [0:BUFFER_SIZE-1];
+    reg [$clog2(BUFFER_SIZE):0] write_ptr;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -24,5 +25,13 @@ module input_buffer1 #(
                 ready_for_processing <= 1;
         end
     end
+
+    // Flatten buffer for external access
+    genvar i;
+    generate
+        for (i = 0; i < BUFFER_SIZE; i++) begin : flatten_buffer
+            assign buffer_flat[(i+1)*DATA_WIDTH-1 -: DATA_WIDTH] = buffer[i];
+        end
+    endgenerate
 
 endmodule
