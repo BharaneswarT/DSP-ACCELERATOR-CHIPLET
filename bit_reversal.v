e4091b0f824a57cea96718921f89c11b9c80766f
SenjuBharani <137296303+BharaneswarT@users.noreply.github.com>
2025-09-22 00:11:37 +0530

    Update bit_reversal.v

--- bit_reversal.v
+++ bit_reversal.v
@@ -2,19 +2,16 @@ module bit_reversal #(
     parameter DATA_WIDTH = 16,
     parameter N = 16
 )(
-    input wire clk,
-    input wire reset,
+    input wire clk, reset,
     input wire start_reorder,
-
-    //flattened inputs: [sample0, sample1, ..., sampleN-1]
-    input wire signed [N*DATA_WIDTH-1:0] real_in,		//concatinated answer but real value
-    input wire signed [N*DATA_WIDTH-1:0] imag_in,		//concatinated answer but imaginary value
+	
+	input wire signed [N*DATA_WIDTH-1:0] real_in,		//cincatinated ans (real value)
+	input wire signed [N*DATA_WIDTH-1:0] imag_in,		//concatinated ans (imaginary val)
 
     output reg signed [N*DATA_WIDTH-1:0] real_out,	//reordered answer of DIT real part
     output reg signed [N*DATA_WIDTH-1:0] imag_out,	//reordered answer of DIT imaginary part
 	 
-	 
-    output reg reorder_done			//signaling to the controller as ocmpleted 
+    output reg reorder_done			//signaling to controller 
 );
 
     localparam INDEX_WIDTH = $clog2(N);		//log2(16)=4bits, to count 0 to 15
@@ -22,14 +19,13 @@ module bit_reversal #(
     integer i, j;   //loop index
     reg [INDEX_WIDTH-1:0] reversed_index;		//holds the bit reversed value of i
 
-    //bit reversal function
-	 
+    //bit reversal function	 
     function [INDEX_WIDTH-1:0] reverse_bits;
         input [INDEX_WIDTH-1:0] in;
         integer k;
         begin
             for (k = 0; k < INDEX_WIDTH; k = k + 1)
-                reverse_bits[k] = in[INDEX_WIDTH-1-k];
+				reverse_bits[k] = in[INDEX_WIDTH-1-k];   //flipping the bit 
         end
     endfunction
 
@@ -43,10 +39,8 @@ module bit_reversal #(
                 reversed_index = reverse_bits(i[INDEX_WIDTH-1:0]);
 
                 // qssign each slice of the output
-                real_out[reversed_index*DATA_WIDTH +: DATA_WIDTH] 
-                    <= real_in[i*DATA_WIDTH +: DATA_WIDTH];
-                imag_out[reversed_index*DATA_WIDTH +: DATA_WIDTH] 
-                    <= imag_in[i*DATA_WIDTH +: DATA_WIDTH];
+                real_out[reversed_index*DATA_WIDTH +: DATA_WIDTH] <= real_in[i*DATA_WIDTH +: DATA_WIDTH];
+				imag_out[reversed_index*DATA_WIDTH +: DATA_WIDTH]  <= imag_in[i*DATA_WIDTH +: DATA_WIDTH];  // i literally have no idea why we have imaginary numbers
             end
             reorder_done <= 1;
         end else begin
@@ -55,4 +49,5 @@ module bit_reversal #(
     end
 
 endmodule
+
 
